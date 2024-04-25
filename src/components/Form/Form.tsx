5c176ad0fa41f2fdafd1fe08f560ceaf68791520
MirMax13 <matskiv2005@gmail.com>
2024-04-25 19:07:33 +0300

    Refactor Form component to use MUI Slider

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,6 +1,11 @@
-import { CHECK_AND_RADIO, RANGE } from "../../data";
+import { CHECK_AND_RADIO } from "../../data";
 import Alert from "../Alert";
 import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Slider from "@mui/material/Slider";
+import Input from "@mui/material/Input";
+
 export interface AI {
   levelOfAI: string[];
   whereAIIsUsed: string[];
@@ -12,7 +17,7 @@ interface IFormProps {
 }
 
 const Form = ({ onSubmit }: IFormProps) => {
-  const [rangeValue, setRangeValue] = React.useState(0);
+  const [rangeValue, setRangeValue] = React.useState<number>(0);
   const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
 
   const defaultFormState = {
@@ -36,12 +41,25 @@ const Form = ({ onSubmit }: IFormProps) => {
     setAI({ ...Ai, TypeOfAI: e.target.value });
   };
 
-  const onRateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseInt(e.target.value);
-    setAI({ ...Ai, rateAIIntelligence: newValue });
-    setRangeValue(newValue);
+  const onRateSliderChange = (event: Event, newValue: number | number[]) => {
+    const value = Array.isArray(newValue) ? newValue[0] : newValue;
+    setAI({ ...Ai, rateAIIntelligence: value });
+    setRangeValue(value);
   };
 
+  const onRateInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value === "" ? 0 : Number(event.target.value);
+    setAI({ ...Ai, rateAIIntelligence: value });
+    setRangeValue(value);
+  };
+
+  const handleBlur = () => {
+    if (rangeValue < 0) {
+      setRangeValue(0);
+    } else if (rangeValue > 100) {
+      setRangeValue(100);
+    }
+  };
   const validateAI = (Ai: AI) => {
     if (
       Ai.levelOfAI.length === 0 ||
@@ -49,6 +67,7 @@ const Form = ({ onSubmit }: IFormProps) => {
       !Ai.TypeOfAI ||
       Ai.rateAIIntelligence === 0
     ) {
+      console.log(Ai);
       return "Please fill all the fields";
     }
   };
@@ -70,6 +89,14 @@ const Form = ({ onSubmit }: IFormProps) => {
     setRangeValue(0);
     setAI(defaultFormState);
     console.log(Ai);
+    document.querySelectorAll('input[type="checkbox"]').forEach((element) => {
+      const checkbox = element as HTMLInputElement;
+      checkbox.checked = false;
+    });
+    document.querySelectorAll('input[type="radio"]').forEach((element) => {
+      const radio = element as HTMLInputElement;
+      radio.checked = false;
+    });
   };
 
   return (
@@ -132,17 +159,80 @@ const Form = ({ onSubmit }: IFormProps) => {
           </div>
           <div className="form-group">
             <label>Rate AI intelligence</label>
-            <div className="range-group">
-              <input
-                type="range"
-                name="rateAIIntelligence"
-                min={RANGE[0].min}
-                max={RANGE[0].max}
-                value={rangeValue}
-                onChange={onRateChange}
-              />
-              <span>{rangeValue}</span>
-            </div>
+
+            <Box sx={{ width: "100%" }}>
+              <Grid container spacing={1} alignItems="center">
+                <Grid item xs>
+                  <Slider
+                    aria-label="Volume"
+                    value={typeof rangeValue === "number" ? rangeValue : 0}
+                    onChange={onRateSliderChange}
+                    aria-labelledby="input-slider"
+                    sx={{
+                      width: "95%",
+                      margin: 0,
+                      "& .MuiSlider-thumb": {
+                        height: 20,
+                        width: 20,
+                        backgroundColor: "#747bff",
+                        "&:hover": {
+                          backgroundColor: "#535bf2",
+                        },
+                        "&.Mui-focusVisible": {
+                          boxShadow:
+                            "0px 0px 0px 8px rgba(116, 123, 255, 0.16)",
+                        },
+                        "&.Mui-active": {
+                          boxShadow:
+                            "0px 0px 0px 14px rgba(116, 123, 255, 0.16)",
+                        },
+                      },
+                      "& .MuiSlider-rail": {
+                        height: 8,
+                        borderRadius: 4,
+                        backgroundColor: "#747bff",
+                      },
+                      "& .MuiSlider-track": {
+                        height: 8,
+                        borderRadius: 4,
+                        backgroundColor: "#747bff",
+                      },
+                    }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Input
+                    value={rangeValue}
+                    size="small"
+                    onChange={onRateInputChange}
+                    onBlur={handleBlur}
+                    inputProps={{
+                      step: 1,
+                      min: 0,
+                      max: 100,
+                      type: "number",
+                      "aria-labelledby": "input-slider",
+                    }}
+                    sx={{
+                      mt: -1,
+                      "& .MuiInputBase-input": {
+                        color: "#213547", // Text color
+                        backgroundColor: "#f9f9f9", // Background color
+                        borderRadius: "4px", // Border radius
+                        padding: "0.5em", // Padding
+                        border: "1px solid #ddd", // Border
+                        "&:hover": {
+                          borderColor: "#747bff", // Border color on hover
+                        },
+                        "&.Mui-focused": {
+                          borderColor: "#747bff", // Border color when focused
+                        },
+                      },
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
           </div>
           <button type="submit" onClick={handleSubmit}>
             Submit
