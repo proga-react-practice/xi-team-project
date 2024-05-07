a75b559fa4155ff26446e2e02a35cc2308771a2c
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-05-08 00:41:41 +0300

    feat: created page with game registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import Form from "./components/Form/Form";
-import Cards from "./components/Cards";
+import Form from "./components/Form/FormGames";
+import Cards from "./components/CardsGames";
+// import Form from "./components/Form/Form";
+// import Cards from "./components/Cards";
 import { useState } from "react";
-import { AI } from "./components/Form/Form";
+// import { AI } from "./components/Form/Form";
 import { Container, Box } from "@mui/material";
-import SmartToyIcon from "@mui/icons-material/SmartToy";
+// import SmartToyIcon from "@mui/icons-material/SmartToy";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import Title from "./components/Title";
 import StyleIcon from "@mui/icons-material/Style";
 import { lightTheme, darkTheme } from "./theme";
 import { ThemeProvider, useTheme } from "@mui/material/styles";
 import Switch from "@mui/material/Switch";
 
+import { Card } from "./components/CardsGames";
+
 function Content() {
   const theme = useTheme();
-  const [formData, setFormData] = useState<AI[]>([]);
-  const handleDelete = (index: number) => {
-    setFormData((prevCards) => prevCards.filter((_, i) => i !== index));
+
+  // Maxym's form
+  // const [formData, setFormData] = useState<AI[]>([]);
+  // const handleDelete = (index: number) => {
+  //   setFormData((prevCards) => prevCards.filter((_, i) => i !== index));
+  // };
+
+  // const handleFormSubmit = (Ai: AI) => {
+  //   setFormData([...formData, Ai]);
+  // };
+
+  // Sofiia's form
+  const [cards, setCards] = useState<Card[]>([]);
+  const handleFormSubmit = (newCard: Card) => {
+    setCards((prevCards) => [...prevCards, newCard]);
   };
 
-  const handleFormSubmit = (Ai: AI) => {
-    setFormData([...formData, Ai]);
+  const deleteCard = (id: string) => {
+    setCards((prevCards) => {
+      const filteredCards = prevCards.filter((card: Card) => card.id !== id);
+      return filteredCards;
+    });
   };
   return (
     <Box
@@ -38,7 +58,12 @@ function Content() {
           paddingRight: { xs: 0, md: 2, lg: 3 },
         }}
       >
-        <Title icon={SmartToyIcon} title="Registration Form" />
+        {/* Maxym's form */}
+        {/* <Title icon={SmartToyIcon} title="Registration Form" />
+        <Form onSubmit={handleFormSubmit} /> */}
+
+        {/* Soffia's form */}
+        <Title icon={SportsEsportsIcon} title="Registration Form" />
         <Form onSubmit={handleFormSubmit} />
       </Container>
       <Container
@@ -50,8 +75,13 @@ function Content() {
           paddingRight: { xs: 0, md: 2, lg: 3 },
         }}
       >
+        {/* Maxyms's form */}
+        {/* <Title icon={StyleIcon} title="Submitted Cards" />
+        <Cards cards={formData} onDelete={handleDelete} /> */}
+
+        {/* Sofiia's form */}
         <Title icon={StyleIcon} title="Submitted Cards" />
-        <Cards cards={formData} onDelete={handleDelete} />
+        <Cards cards={cards} onDelete={deleteCard} />
       </Container>
     </Box>
   );
---
+++ src/components/Form/FormGames.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import { Card } from "../CardsGames";
+import Alert from "../Alert";
+import { TextField, Select, MenuItem, Box } from "@mui/material";
+import { InputLabel, Button, Chip } from "@mui/material";
+import { Container, Typography, FormControl } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import ClearIcon from "@mui/icons-material/Clear";
+import SendIcon from "@mui/icons-material/Send";
+
+export interface Props {
+  onSubmit: (data: Card) => void;
+}
+
+const Form: React.FC<Props> = ({ onSubmit }) => {
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+  const [name, setName] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [price, setPrice] = useState<number | "">("");
+  const [currency, setCurrency] = useState("");
+
+  const theme = useTheme();
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setErrorMessage("");
+
+    if (name === "" || difficulty === "" || price === "") {
+      setErrorMessage("Please fill all the fields");
+      return;
+    } else {
+      onSubmit({ id: crypto.randomUUID(), name, difficulty, price, currency });
+    }
+  };
+
+  const handleClear = () => {
+    setName("");
+    setDifficulty("");
+    setPrice("");
+    setCurrency("");
+  };
+
+  return (
+    <>
+      {errorMessage && (
+        <Alert message={errorMessage} onClose={() => setErrorMessage(null)} />
+      )}
+      <Container
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          border: "2px solid",
+          borderColor: theme.palette.text.primary,
+          borderRadius: 2,
+          justifyContent: "left",
+          bgcolor: theme.palette.background.paper,
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.65em",
+        }}
+      >
+        <Typography variant="h3" sx={{ my: 2, textAlign: "center" }}>
+          Register the Game
+        </Typography>
+
+        <TextField
+          fullWidth
+          label="Name of the Game"
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Difficulty</InputLabel>
+          <Select
+            label="Difficulty"
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            renderValue={(selected) => (
+              <Chip
+                label={selected}
+                sx={{
+                  height: "1.8rem",
+                  padding: { xs: 0.5, sm: 1, md: 1.5 },
+                }}
+              />
+            )}
+          >
+            <MenuItem value={"Easy"}>Easy</MenuItem>
+            <MenuItem value={"Medium"}>Medium</MenuItem>
+            <MenuItem value={"Hard"}>Hard</MenuItem>
+            <MenuItem value={"Expert"}>Expert</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Container
+          disableGutters
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: 1,
+            gap: 1,
+          }}
+        >
+          <TextField
+            fullWidth
+            sx={{ width: "130%" }}
+            label="Price"
+            type="number"
+            variant="outlined"
+            value={price}
+            onChange={(e) =>
+              setPrice(
+                e.target.value === ""
+                  ? ""
+                  : Math.max(0, parseInt(e.target.value))
+              )
+            }
+          />
+
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Currency</InputLabel>
+
+            <Select
+              label="Currency"
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+              renderValue={(selected) => (
+                <Chip
+                  label={selected}
+                  sx={{
+                    height: "1.8rem",
+                    padding: { xs: 0.5, sm: 1, md: 1.5 },
+                  }}
+                />
+              )}
+            >
+              <MenuItem value={"₴"}>₴</MenuItem>
+              <MenuItem value={"$"}>$</MenuItem>
+              <MenuItem value={"€"}>€</MenuItem>
+            </Select>
+          </FormControl>
+        </Container>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: 2,
+            gap: 1,
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{ px: 5 }}
+            startIcon={<ClearIcon />}
+            onClick={handleClear}
+          >
+            Clear
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ px: 10 }}
+            endIcon={<SendIcon />}
+            onClick={handleSubmit}
+          >
+            Add
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Form;
