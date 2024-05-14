8821637d8fb48c928bd9e4bea776234fb22a7dc7
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-05-14 03:40:26 +0300

    fix: deleted unused lines

--- src/components/Games/Form.tsx
+++ src/components/Games/Form.tsx
@@ -14,8 +14,8 @@ const size = 17;
 
 export interface Props {
   onSubmit: (data: Card) => void;
-  editCard?: Card | null; // Allow null value for editCard
-  onCancel: () => void; // Add onCancel function
+  editCard?: Card | null;
+  onCancel: () => void;
 }
 
 const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
@@ -38,7 +38,6 @@ const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
   const { register, control, handleSubmit, formState, reset, setValue } = form;
   const { errors } = formState;
 
-  // Populate default values if editCard prop is provided
   useEffect(() => {
     if (editCard) {
       reset(editCard);
@@ -47,10 +46,8 @@ const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
 
   const onSubmitHandler = (data: Card) => {
     if (editCard) {
-      // If editCard exists, it means we're editing an existing card
       onSubmit({ ...data, id: editCard.id });
     } else {
-      // If editCard doesn't exist, it means we're adding a new card
       onSubmit({ ...data, id: crypto.randomUUID() });
     }
   };
@@ -65,7 +62,6 @@ const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
   };
 
   const handleCancel = () => {
-    // Clear the form and cancel editing
     clearForm();
     onCancel();
   };
@@ -95,7 +91,7 @@ const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
           fullWidth
           label={placeholders.name}
           variant="outlined"
-          value={form.watch("name")} // Bind value to form state
+          value={form.watch("name")}
           {...register("name", {
             required: { value: true, message: "Name is required" },
             maxLength: {
@@ -113,7 +109,7 @@ const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
           <InputLabel id="difficulty-select">Difficulty</InputLabel>
           <Select
             label={placeholders.difficulty}
-            value={form.watch("difficulty")} // Bind value to form state
+            value={form.watch("difficulty")}
             {...register("difficulty", {
               required: { value: true, message: "Difficulty is required" },
               onChange: (e) => setValue("difficulty", e.target.value),
@@ -167,7 +163,7 @@ const Form: React.FC<Props> = ({ onSubmit, editCard, onCancel }) => {
             <InputLabel id="currency-select">Currency</InputLabel>
             <Select
               label={placeholders.currency}
-              value={form.watch("currency")} // Bind value to form state
+              value={form.watch("currency")}
               {...register("currency", {
                 required: { value: true, message: "Currency is required" },
                 onChange: (e) => setValue("currency", e.target.value),
