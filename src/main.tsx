e17dbbc854680f5779b2c8790b7c9a00fbfc045a
Maxym Matskiv <matskiv2005@gmail.com>
2024-05-21 14:13:49 +0300

    Replaced margin style from main.tsx to index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import router from "./router.tsx";
 import { RouterProvider } from "react-router-dom";
-import { Box } from "@mui/material";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Box
-      sx={{
-        margin: -1,
-      }}
-    >
-      <RouterProvider router={router} />
-    </Box>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
