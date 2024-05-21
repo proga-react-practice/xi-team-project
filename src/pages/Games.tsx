4a2cd1d200a1fb12d77ad53ba21b7c88a3bb5ec7
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-05-21 17:00:34 +0300

    fix: changed form width

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -15,18 +15,28 @@ export default function Games() {
       <Box
         sx={{
           display: "flex",
-          flexDirection: { xs: "column", md: "row" },
+          flexDirection: { xs: "column", sm: "column", md: "row" },
           justifyContent: "space-between",
-          alignItems: "flex-start",
+          // alignItems: "flex-start",
           backgroundColor: theme.palette.background.default,
           minHeight: "100vh",
-          padding: theme.spacing(2),
-          boxSizing: "border-box",
         }}
       >
+        {/* <Box
+      sx={{
+        flexDirection: { xs: "column", sm: "column", md: "row" },
+        display: "flex",
+        justifyContent: "space-between",
+        bgcolor: theme.palette.background.default,
+        // TODO fix layout
+        width: "98.9vw",
+        maxWidth: "100%",
+        minHeight: "100vh",
+      }}
+    ></Box> */}
         <Container
           sx={{
-            width: { xs: "100%", md: "40%" },
+            width: { xs: "100%", md: "35%" },
             padding: theme.spacing(2),
             boxSizing: "border-box",
           }}
