9eb1fe6819184db95683cffce593d794bbcc24ed
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-05-23 13:22:41 +0300

    feat: added tab for mix-cards

--- src/components/Menu/Tabs.ts
+++ src/components/Menu/Tabs.ts
@@ -2,4 +2,5 @@ export const tabs = [
   { label: "Home", value: "/" },
   { label: "Games", value: "/games" },
   { label: "Artificial Intelligence", value: "/AI" },
+  { label: "Mix Cards", value: "/mix-cards" },
 ];
