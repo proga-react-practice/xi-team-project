f1350a6aeed7fadcac7866000be63472f7df9018
Sofiia Stanishevska <sophiyastanish@gmail.com>
2024-05-23 19:11:59 +0300

    fix: added new component

--- src/components/Games/Cards.tsx
+++ src/components/Games/Cards.tsx
@@ -191,4 +191,65 @@ const CardsList: React.FC = () => {
   );
 };
 
+export const CardComponentMix: React.FC<{ card: Card }> = ({ card }) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        flexDirection: "column",
+        paddingTop: 2,
+        paddingBottom: 2,
+        // paddingLeft: { md: 0, lg: 2 },
+        paddingRight: { md: 0, lg: 8 },
+        maxWidth: { md: "90%", lg: "80%" },
+        border: "2px solid",
+        borderColor: theme.palette.text.primary,
+        py: 2.5,
+        // px: 2,
+        borderRadius: 2,
+        position: "relative",
+        marginBottom: 5,
+        bgcolor: "background.paper",
+        "&::after": {
+          content: '""',
+          position: "absolute",
+          bottom: theme.spacing(1),
+          left: {
+            xs: theme.spacing(1),
+            sm: theme.spacing(2),
+            md: theme.spacing(4),
+          },
+          right: {
+            xs: theme.spacing(0.7),
+            sm: theme.spacing(1),
+            md: theme.spacing(2),
+          },
+          height: "2px",
+          backgroundColor: theme.palette.text.primary,
+        },
+      }}
+    >
+      <CardsInfo title="Name of the Game" info={card.name} />
+      <CardsInfo title="Difficulty" info={card.difficulty} />
+      <CardsInfo
+        title="Price"
+        info={card.price.toString() + " " + card.currency}
+      />
+      {/* <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          paddingRight: {
+            xs: 2,
+            sm: 4,
+            md: 6,
+            lg: 10,
+          },
+        }}
+      ></Box> */}
+    </Box>
+  );
+};
+
 export { CardsList as Cards };
