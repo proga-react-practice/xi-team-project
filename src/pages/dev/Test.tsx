3cd047cd8bcda4a7d2c557b35641376aad7348b2
yu_shmbr <ura.shambora@gmail.com>
2024-05-25 13:53:08 +0300

    Dummy dnd

--- src/pages/dev/Test.tsx
+++ src/pages/dev/Test.tsx
@@ -1,26 +1,27 @@
-import type { CSSProperties, FC } from 'react'
-import { memo } from 'react'
-import { useDrag, useDrop } from 'react-dnd'
-
+import type { CSSProperties, FC } from "react";
+import { memo } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import { CardComponent } from "../../components/Games/Cards";
+import { CardsProvider } from "../../components/context/GamesCardsContextProvider";
 
 const style: CSSProperties = {
-  border: '1px dashed gray',
-  padding: '0.5rem 1rem',
-  marginBottom: '.5rem',
-  backgroundColor: 'white',
-  cursor: 'move',
-}
+  border: "1px dashed gray",
+  padding: "0.5rem 1rem",
+  marginBottom: ".5rem",
+  backgroundColor: "white",
+  cursor: "move",
+};
 
 export interface CardProps {
-  id: string
-  text: string
-  moveCard: (id: string, to: number) => void
-  findCard: (id: string) => { index: number }
+  id: string;
+  text: string;
+  moveCard: (id: string, to: number) => void;
+  findCard: (id: string) => { index: number };
 }
 
 interface Item {
-  id: string
-  originalIndex: number
+  id: string;
+  originalIndex: number;
 }
 
 export const Card: FC<CardProps> = memo(function Card({
@@ -29,7 +30,7 @@ export const Card: FC<CardProps> = memo(function Card({
   moveCard,
   findCard,
 }) {
-  const originalIndex = findCard(id).index
+  const originalIndex = findCard(id).index;
   const [{ isDragging }, drag] = useDrag(
     () => ({
       type: "card",
@@ -38,33 +39,43 @@ export const Card: FC<CardProps> = memo(function Card({
         isDragging: monitor.isDragging(),
       }),
       end: (item, monitor) => {
-        const { id: droppedId, originalIndex } = item
-        const didDrop = monitor.didDrop()
+        const { id: droppedId, originalIndex } = item;
+        const didDrop = monitor.didDrop();
         if (!didDrop) {
-          moveCard(droppedId, originalIndex)
+          moveCard(droppedId, originalIndex);
         }
       },
     }),
-    [id, originalIndex, moveCard],
-  )
+    [id, originalIndex, moveCard]
+  );
 
   const [, drop] = useDrop(
     () => ({
       accept: "card",
       hover({ id: draggedId }: Item) {
         if (draggedId !== id) {
-          const { index: overIndex } = findCard(id)
-          moveCard(draggedId, overIndex)
+          const { index: overIndex } = findCard(id);
+          moveCard(draggedId, overIndex);
         }
       },
     }),
-    [findCard, moveCard],
-  )
+    [findCard, moveCard]
+  );
 
-  const opacity = isDragging ? 0 : 1
+  const opacity = isDragging ? 0 : 1;
   return (
     <div ref={(node) => drag(drop(node))} style={{ ...style, opacity }}>
-      {text}
+      <CardsProvider>
+        <CardComponent
+          card={{
+            id: "string",
+            name: "string",
+            difficulty: "string",
+            price: 123,
+            currency: "string",
+          }}
+        />
+      </CardsProvider>
     </div>
-  )
-})
+  );
+});
