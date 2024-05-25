4d483f24c6d06d1bd798576371ad0e6a90b01b62
yu_shmbr <ura.shambora@gmail.com>
2024-05-25 13:38:11 +0300

    Add lost exports to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import router from "./router.tsx";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { AICardsProvider } from "./components/context/AICardsContextProvider.tsx";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <AICardsProvider>
